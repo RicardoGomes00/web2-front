558e7a2a28e727f9b7a4cefa0138d48d94ede9e4
giovanitrier <giovanitrier@gmail.com>
2024-03-29 00:39:09 -0300

    Pagina Inicial atualizada, front-end da tela pagamento-pedido incluido

---
+++ we2-front/src/app/Cliente/pagamento-pedido/pagamento-pedido.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagamento-pedido',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagamento-pedido.component.html',
+  styleUrl: './pagamento-pedido.component.css'
+})
+export class PagamentoPedidoComponent {
+
+}
