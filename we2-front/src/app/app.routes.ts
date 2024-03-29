fbb359dbc0da1208dd2ee404a180f2980a797fde
Aline Machado Lima <machadolima@ufpr.br>
2024-03-28 22:37:22 -0300

    modifica nome componente

--- we2-front/src/app/app.routes.ts
+++ we2-front/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
-import { LoginComponent } from './seguranca/login/login.component';
+import { LoginComponent } from './login/login.component';
 import { AutocadastroComponent } from './autocadastro/autocadastro.component';
 import { OrderOnlineComponent } from './order-online/order-online.component';
 import { PaginaInicialComponent } from './pagina_inicial/pagina_inicial.component';
