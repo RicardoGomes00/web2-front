ac06a9baaa106ae88f488e1caa8484b1fa190703
Ricardo Gomes <rico00gomes@gmail.com>
2024-03-28 23:42:49 -0300

    add changes rf05/rf06

---
+++ we2-front/src/app/Cliente/aprovacao-orcamento/aprovacao-orcamento.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aprovacao-orcamento',
+  standalone: true,
+  imports: [],
+  templateUrl: './aprovacao-orcamento.component.html',
+  styleUrl: './aprovacao-orcamento.component.css'
+})
+export class AprovacaoOrcamentoComponent {
+
+}
