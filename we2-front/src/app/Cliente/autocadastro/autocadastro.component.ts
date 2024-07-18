1e225b0370a97d2cf69e661eda8956d6fbee325e
Ricardo Gomes <rico00gomes@gmail.com>
2024-07-18 17:27:26 -0300

    fix: mask

--- we2-front/src/app/Cliente/autocadastro/autocadastro.component.ts
+++ we2-front/src/app/Cliente/autocadastro/autocadastro.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { NgxMaskDirective } from 'ngx-mask';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
 
 @Component({
   selector: 'app-autocadastro',
+  standalone: true,
   templateUrl: './autocadastro.component.html',
-  styleUrl: './autocadastro.component.css'
+  styleUrl: './autocadastro.component.css',
+  imports: [
+    NgxMaskDirective,
+  ],
 })
 export class AutocadastroComponent {
 
+  cadastroForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.cadastroForm = this.fb.group({
+      cpf: ['', [Validators.required]],
+      nome: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      endereco: ['', [Validators.required]],
+      telefone: ['', [Validators.required, Validators.pattern(/\(\d{2}\) \d{4,5}-\d{4}/)]]
+    });
+  }
+
+  get nome() {
+    return this.cadastroForm.get('nome');
+  }
+
+  get email() {
+    return this.cadastroForm.get('email');
+  }
+
+  get telefone() {
+    return this.cadastroForm.get('telefone');
+  }
+
+  onSubmit() {
+    if (this.cadastroForm.valid) {
+      console.log(this.cadastroForm.value);
+    }
+  }
 }
--- we2-front/src/app/app.config.ts
+++ we2-front/src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideNgxMask } from 'ngx-mask';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
-    provideHttpClient()
+    provideHttpClient(),
+    provideNgxMask(),
   ],
 };
