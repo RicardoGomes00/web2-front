dd2136927343f43618f088b1a79f65a7f696c632
Ricardo Gomes <rico00gomes@gmail.com>
2024-07-25 15:09:41 -0300

    fix: concluido relatorios

--- we2-front/src/app/Funcionario/relatorios/relatorio.service.ts
+++ we2-front/src/app/Funcionario/relatorios/relatorio.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import jspdf from 'jspdf';
-import autoTable from 'jspdf-autotable'
+import autoTable from 'jspdf-autotable';
 
 @Injectable({
   providedIn: 'root'
