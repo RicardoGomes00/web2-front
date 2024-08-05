7d707573eb896b5d54e61d43a4df6333128faea7
jeffersonhoffmann <jeffersonhoffmann1@gmail.com>
2024-08-05 20:57:48 -0300

    Dashboard fixo para cliente e funcionário

--- we2-front/src/app/Cliente/dashboard-cliente/dashboard-cliente.component.ts
+++ we2-front/src/app/Cliente/dashboard-cliente/dashboard-cliente.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PaginaInicialClienteComponent } from '../pagina-inicial-cliente/pagina-inicial-cliente.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-cliente',
   standalone: true,
-  imports: [ CommonModule, PaginaInicialClienteComponent, RouterModule ],
+  imports: [ CommonModule, RouterModule],
   templateUrl: './dashboard-cliente.component.html',
   styleUrl: './dashboard-cliente.component.css'
 })
